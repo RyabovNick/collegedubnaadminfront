6c1240936ec415cd048d7feec0a13d001096b9f6
Nick <nikitos.aka.zed@gmail.com>
2018-07-31 12:00:54 +0300

    eduStandats, Grants, Vacant added

--- app/src/routes.js
+++ app/src/routes.js
@@ -7,13 +7,13 @@ import Common from "./components/sveden/Common.vue";
 import Struct from "./components/sveden/Struct.vue";
 import Document from "./components/sveden/Document.vue";
 import Education from "./components/sveden/Education.vue";
-/*import EduStandarts from "./components/sveden/EduStandarts.vue"*/
+import EduStandarts from "./components/sveden/EduStandarts.vue";
 import Employees from "./components/sveden/Employees.vue";
-/*import Objects from "./components/sveden/Objects.vue"
-import Grants from "./components/sveden/Grants.vue"*/
+/*import Objects from "./components/sveden/Objects.vue"*/
+import Grants from "./components/sveden/Grants.vue";
 import PaidEdu from "./components/sveden/PaidEdu.vue";
 import Budget from "./components/sveden/Budget.vue";
-/*import Vacant from "./components/sveden/Vacant.vue"*/
+import Vacant from "./components/sveden/Vacant.vue";
 
 export const routes = [
     { path: "/", name: "Home", component: Home },
@@ -29,15 +29,15 @@ export const routes = [
         component: Document
     },
     { path: "/sveden/education", name: "Education", component: Education },
-    /*{ path: '/sveden/eduStandarts', name: 'EduStandarts', component: EduStandarts },*/ {
+    { path: "/sveden/eduStandarts", name: "EduStandarts", component: EduStandarts },
+    {
         path: "/sveden/employees",
         name: "Employees",
         component: Employees
     },
-    /*{ path: '/sveden/objects', name: 'Objects', component: Objects },
-    { path: '/sveden/grants', name: 'Grants', component: Grants },
-    */
-
+    /*{ path: '/sveden/objects', name: 'Objects', component: Objects },*/
+    { path: "/sveden/grants", name: "Grants", component: Grants },
     { path: "/sveden/paid_edu", name: "PaidEdu", component: PaidEdu },
-    { path: "/sveden/budget", name: "Budget", component: Budget }
+    { path: "/sveden/budget", name: "Budget", component: Budget },
+    { path: "/sveden/vacant", name: "Vacant", component: Vacant }
 ];
