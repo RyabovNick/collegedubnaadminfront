3cfce69145109b08913834c9ac32609dec396b76
Nick <nikitos.aka.zed@gmail.com>
2018-08-09 17:52:29 +0300

    started admin panel app creation

---
+++ unic-ad-panel/src/main.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import axios from "axios";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
+Vue.use(Vuetify);
+
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount("#app");
