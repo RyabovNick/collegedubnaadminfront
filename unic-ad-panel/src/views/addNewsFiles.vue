4bc3973052b288e092520a0bae29fdddb8368f55
Ryabov Nikita <nikitos.aka.zed@gmail.com>
2019-03-12 18:47:01 +0300

    news files delete added

--- unic-ad-panel/src/views/addNewsFiles.vue
+++ unic-ad-panel/src/views/addNewsFiles.vue
@@ -35,6 +35,27 @@
         </v-flex>
       </v-layout>
     </v-container>
+    <section v-if="errored">
+      <p>Приносим извинения, произошла ошибка. Пожалуйста, повторите позднее</p>
+    </section>
+    <section v-else>
+      <div v-if="loading">Загрузка...</div>
+      <v-toolbar flat color="white">
+        <v-toolbar-title>Удаление фото у новостей</v-toolbar-title>
+        <v-divider class="mx-2" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details></v-text-field>
+      </v-toolbar>
+      <v-data-table :headers="headers" :items="photos" :search="search" class="elevation-1">
+        <template slot="items" slot-scope="props">
+          <td class="text-xs-left">{{ props.item.idnews }}</td>
+          <td class="text-xs-left">{{ props.item.link }}</td>
+          <td class="justify-center layout px-0">
+            <v-icon v-if="props.item.id !== null" small @click="deleteItem(props.item)">delete</v-icon>
+          </td>
+        </template>
+      </v-data-table>
+    </section>
     <v-snackbar v-model="snackbar" :color="color" :timeout="50 * 100">
       {{ text }}
       <v-btn dark flat @click="snackbar = false">Close</v-btn>
@@ -47,12 +68,34 @@ import { mapGetters } from "vuex";
 import {
   FETCH_LIST_NEWS,
   FETCH_NEWS_PHOTO,
-  UPLOAD_NEWS_PHOTO
+  UPLOAD_NEWS_PHOTO,
+  DELETE_NEWS_PHOTO
 } from "@/store/actions.type";
 
 export default {
   data() {
     return {
+      loading: false,
+      errored: false,
+      search: "",
+      headers: [
+        { text: "ID новости", value: "idnews" },
+        { text: "Ссылка", value: "link" },
+        { text: "Действия", value: "actions" }
+      ],
+
+      editedIndex: -1,
+      editedItem: {
+        id: 0,
+        idnews: "",
+        link: ""
+      },
+      defaultItem: {
+        id: 0,
+        idnews: "",
+        link: ""
+      },
+
       selectedValue: null,
       //snackbar
       snackbar: false,
@@ -66,6 +109,7 @@ export default {
   methods: {
     fetchNews() {
       this.$store.dispatch(FETCH_LIST_NEWS);
+      this.$store.dispatch(FETCH_NEWS_PHOTO);
       //this.$store.dispatch(FETCH_NEWS_PHOTO);
     },
     async uploadFile(file) {
@@ -87,10 +131,40 @@ export default {
         this.text = "Приносим извинения, произошла ошибка";
         this.snackbar = true;
       }
+    },
+    async deleteItem(item) {
+      const index = this.photos.indexOf(item);
+      const id = item.id;
+      try {
+        (await confirm("Действительно хотите удалить элемент с ID: " + id)) &&
+          this.$store.dispatch(DELETE_NEWS_PHOTO, { id }).then(() => {
+            this.photos.splice(index, 1);
+            this.color = "success";
+            this.text = "Данные успешно изменены";
+            this.snackbar = true;
+          });
+      } catch {
+        this.color = "error";
+        this.text = "Произошла ошибка при изменении данных";
+        this.snackbar = true;
+      }
+    },
+
+    close() {
+      this.dialog = false;
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
     }
   },
   computed: {
     ...mapGetters(["photos", "listNews"])
+  },
+  watch: {
+    dialog(val) {
+      val || this.close();
+    }
   }
 };
 </script>
